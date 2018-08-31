d30fdef91a25beecfff21cf62bcb5a33cdf8d962
tareifz <root@tareifz.me>
2018-08-31 16:28:58 +0300

    new font & new theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -77,10 +77,11 @@
 ;; [themes]
 ;; (use-package sourcerer-theme)
 ;; (use-package dracula-theme)
-(use-package atom-one-dark-theme)
+;; (use-package atom-one-dark-theme)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 ;; (load-file "~/.emacs.d/themes/tareifz-basic-theme.el")
 ;; (load-file "~/.emacs.d/themes/tareifz-shadows-theme.el")
+(load-file "~/.emacs.d/themes/lorisan-theme.el")
 
 (defun load-only-theme ()
 	"Disable all themes and then load a single theme interactively."
@@ -333,8 +334,10 @@
 ;; (set-frame-font "Ubuntu Mono")
 ;; (set-frame-font "More Perfect DOS VGA")
 ;; (set-frame-font "Unifont")
-(set-frame-font "Terminus:antialias=none")
-(set-face-attribute 'default nil :height 120)
+;; (set-frame-font "Terminus:antialias=none")
+;; (set-face-attribute 'default nil :height 120)
+;; (set-frame-font "Monaco")
+(set-frame-font "Consolas")
 
 (setq-default custom-file "~/.emacs.d/auto-generated-customized-settings.el")
 (load-file custom-file)
