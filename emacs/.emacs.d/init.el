d1b9c632e582b85ee843439e19b0dc56e6abd9bb
tareifz <root@tareifz.me>
2018-11-17 10:09:23 +0300

    Tangle init.org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,4 @@
-;;; package --- summery
-;;; Commentary:
 
-;; ######## Packages & Package Manager ######## ;;
-
-;;; Code:
-;; ---------- Initializing ---------- ;;
 (require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -18,262 +12,37 @@
     (package-install 'use-package)))
 
 (require 'use-package)
+
 (require 'bind-key)
 
-;; install packages if not in the system.
 (setq use-package-always-ensure t)
 
-(use-package try)
+(setq user-full-name "Tareif Al-Zamil"
+      user-mail-address "root@tareifz.me")
 
-;; [smartparens]
-;; auto-close parens.
-(use-package smartparens
-  :config
-  (smartparens-global-mode t))
-;; [rainbow-mode]
-;; represent colors with the color they represent as background.
-(use-package rainbow-mode
-  :config
-  (add-hook 'prog-mode-hook #'rainbow-mode))
-;; [rainbow-delimiters]
-;; rainbow parentheses.
-(use-package rainbow-delimiters
-  :requires rainbow-mode
-  :config
-  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
-;; [aggressive-indent]
-;; minor mode that keeps your code always indented.
-(use-package aggressive-indent
-  :config
-  (global-aggressive-indent-mode t))
-;; [beacon]
-;; shows the cursor position.
-(use-package beacon
-  :custom
-  (beacon-mode t "turn on beacon mode.")
-  (beacon-blink-when-focused t "let the cursor blink when focused."))
-;; [smex]
-;; commands bar auto complete and suggestion.
-(use-package smex
-  :bind (("M-x" . smex))
-  :config (smex-initialize))
-;; [company]
-;; auto-complete engine.
-(use-package company
-  :config
-  (global-company-mode t))
-;; [rust-mode]
-;; rust-lang support.
-(use-package rust-mode
-  :custom
-  (company-tooltip-align-annotations t))
-;; [racer]
-;; provide rust code completion with Company.
-;; rustup component add rust-src
-;; cargo install racer
-(use-package racer
-  :requires rust-mode
-  :config
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode))
-;; [themes]
-;; (use-package sourcerer-theme)
-;; (use-package dracula-theme)
-;; (use-package atom-one-dark-theme)
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;; (load-file "~/.emacs.d/themes/tareifz-basic-theme.el")
-;; (load-file "~/.emacs.d/themes/tareifz-shadows-theme.el")
-(use-package hydandata-light-theme)
-
-(defun load-only-theme ()
-  "Disable all themes and then load a single theme interactively."
-  (interactive)
-  (while custom-enabled-themes
-    (disable-theme (car custom-enabled-themes)))
-  (call-interactively 'load-theme))
-;; [org-mode]
-(use-package org)
-;; [multiple-cursors]
-(use-package multiple-cursors
-  :config
-  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
-;; [switch-window]
-(use-package switch-window
-  :config
-  (global-set-key (kbd "C-x o") 'switch-window))
-;; [web-mode]
-(use-package web-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
-
-(use-package js2-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
-;; [electric-operator]
-(use-package electric-operator
-  :config
-  (add-hook 'rust-mode-hook #'electric-operator-mode)
-  (add-hook 'javascript-mode-hook #'electric-operator-mode)
-  (electric-operator-add-rules-for-mode 'rust-mode
-                                        (cons "->" " -> ")))
-;; [impatient-mode]
-;; M-x httpd-start
-;; M-x impatient-mode
-;; http://localhost:8080/imp/
-(use-package impatient-mode)
-;; [all-the-icons]
-(use-package all-the-icons)
-;; M-x all-the-icons-install-fonts
-;; [neotree]
-(use-package neotree
-  :bind(("<f5>" . neotree-toggle))
-  :config
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-  (setq-default neo-show-hidden-files t))
-;; [symon] {Disabled}
-;; good feature but annoying some times.
-;; (use-package symon
-;;	:config
-;;	(symon-mode 1))
-;; [telephone-line]
-(use-package telephone-line
-  :config
-  (telephone-line-mode 1))
-;; [which-key]
-(use-package which-key
-  :config
-  (which-key-mode)
-  (which-key-setup-side-window-right-bottom))
-;; [expand-region]
-(use-package expand-region
-  :bind ("C-=" . er/expand-region))
-;; [flycheck]
-(use-package flycheck
-  :init (global-flycheck-mode)
-  :config
-  (flycheck-add-mode 'javascript-eslint 'web-mode))
-;; [flycheck-inline] {Disabled}
-;; annoying with long error messages (rustc errors)
-;; (use-package flycheck-inline
-;;	:requires (flycheck)
-;;	:config	(flycheck-inline-mode 1))
-;; [flycheck-rust]
-(use-package flycheck-rust
-  :requires (flycheck)
-  :hook (rust-mode . flycheck-rust-setup))
-;; [geiser]
-;; scheme tools for Emacs.
-;; C-c C-b === eval-buffer
-;; C-c C-c === eval-definition.
-(use-package geiser
-  :config
-  (setq geiser-active-implementations '(guile)))
-
-;; [flymd]
-;; Live markdown preview
-(use-package flymd
-  :config
-  (setq-default flymd-output-directory "~/.flymd/"))
-
-;; [helm]
-;; Search, completion ...
-(use-package helm
-  :init
-  (setq helm-mode-fuzzy-match        t
-        helm-buffers-fuzzy-matching  t
-        helm-recentf-fuzzy-match     t
-        helm-M-x-fuzzy-match         t
-        helm-full-frame              nil
-        helm-ff-guess-ffap-urls      nil
-        helm-ff-guess-ffap-filenames nil
-        helm-highlight-matches-around-point-max-lines 0)
-  :bind (("M-x" . helm-M-x)
-         ("C-x b" . helm-buffers-list)
-         ("C-x C-f" . helm-find-files)
-         ("C-x C-d" . helm-browse-project)
-         ("C-c f" . helm-recentf)
-         ("M-y" . helm-show-kill-ring)
-         :map helm-map
-         ("<tab>" . helm-execute-persistent-action)	;; make tab complete action
-         ("C-i" . helm-execute-persistent-action)
-         ("C-z" . helm-select-action)
-         ))
-
-;; ;;(global-set-key (kbd "<f5> s")                       'helm-find)
-
-(use-package helm-ls-git
-  :requires (helm))
-
-(use-package emmet-mode
-  :hook (web-mode . emmet-mode))
-
-(use-package diff-hl
-  :config
-  (global-diff-hl-mode)
-  (diff-hl-flydiff-mode))
-
-;; ############# Custom Functions ############# ;;
-(defun tareifz/kill-line ()
-  "Killing current line with the new line character, and put the cursor at the beginning of the line."
-  (interactive)
-  (let (line-begin line-end)
-    (setq line-begin (line-beginning-position))
-    (setq line-end (if (= (point-max) (line-end-position))
-                       (progn
-                         (line-end-position))
-                     (progn
-                       (forward-line 1)
-                       (line-beginning-position))))
-    (kill-region line-begin line-end)
-    (beginning-of-line))
-  )
-;; ############# Editer User Info ############# ;;
-(setq user-full-name "Tareif Al-Zamil")
-(setq user-mail-address "root@tareifz.me")
-
-;; ######### Editer General Settings ########## ;;
-;; Ask "y" or "n" instead of "yes" or "no". Yes, laziness is great.
 (fset 'yes-or-no-p 'y-or-n-p)
-;; Highlight corresponding parentheses when cursor is on one
+
 (show-paren-mode t)
-;; Highlight tabulations
+
 (setq-default highlight-tabs t)
-;; Show trailing white spaces
+
 (setq-default show-trailing-whitespace t)
-;; Remove useless whitespace before saving a file
+
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'before-save-hook (lambda() (delete-trailing-whitespace)))
-;; Skip splash screen
+
 (setq inhibit-splash-screen t
       initial-scratch-message nil
       initial-major-mode 'org-mode)
-;; Show empty line markers
-;;(setq-default indicate-empty-lines t)
-;;(when (not indicate-empty-lines)
-;;(toggle-indicate-empty-lines))
 
-;; Make ibuffer default
 (defalias 'list-buffers 'ibuffer)
 
-;; Save/restore opened files and windows configurations
 (desktop-save-mode 1)
 
-;; make typing delete/overwrites selected text
 (delete-selection-mode 1)
 
-;; turn on highlighting current line
 (global-hl-line-mode 1)
 
-;; when a file is updated outside emacs, make it update if it's already opened in emacs
 (global-auto-revert-mode 1)
 
 (defun my-prettify-symbols-list ()
@@ -295,27 +64,13 @@
 ;; display “lambda” as “λ”
 (global-prettify-symbols-mode 1)
 
-;; override [C-k] (kill-to-end-of-line)
-;; the bind-key* (with star) will override key bindings in all minor modes
-(bind-key* "C-k" 'tareifz/kill-line)
-;; ################ Tabs Setup ################ ;;
-;; Indent using 2 spaces tab
 (setq-default indent-tabs-mode nil)
+
 (setq-default tab-width 2)
 (setq-default default-tab-width 2)
 
 (setq-default js-indent-level 2)
 
-;; insert literal tab instead of invoking indentation command
-(defun my-insert-tab-char ()
-  "Insert a tab char. (ASCII 9, \t)"
-  (interactive)
-  (insert "\t"))
-
-;; (global-set-key (kbd "<tab>") 'my-insert-tab-char) ; same as Ctrl+i
-;; make tab key call indent command or insert tab character, depending on cursor position
-(setq-default tab-always-indent nil)
-;; ############## Ido Mode Setup ############## ;;
 (progn
   ;; make buffer switch command do suggestions, also for find-file command
   (require 'ido)
@@ -345,33 +100,29 @@
 (require 'ido)
 ;; stop ido suggestion when doing a save-as
 (define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)
-;; ############### Backup Files ############### ;;
-;; Remove all backup files
+
 (setq make-backup-files nil)
 (setq backup-inhibited t)
 (setq auto-save-default nil)
-;; ############## Files Encoding ############## ;;
-;; Set locale to UTF8
+
 (set-language-environment 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (setq locale-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
-;; ############# Globel Variables ############# ;;
 
-;; Disable menu bar
 (menu-bar-mode -1)
-;; Disable tool bar
+
 (tool-bar-mode -1)
-;; Disable scroll bar
+
 (toggle-scroll-bar -1)
-;; Display line number
+
 (global-linum-mode t)
 (setq-default linum-format " %d ")
-;; Every time bookmark is changed, automatically save it
+
 (setq bookmark-save-flag 1)
-;; font settings
+
 ;; (set-frame-font "Ubuntu Mono")
 ;; (set-frame-font "More Perfect DOS VGA")
 ;; (set-frame-font "Unifont")
@@ -381,13 +132,202 @@
 (set-frame-font "Hack")
 (set-face-attribute 'default nil :height 120)
 
-
-;; for arch linux
-;; (set-frame-font "xos4 Terminus:antialias=none")
-;; (set-face-attribute 'default nil :height 120)
-
-
 (setq-default custom-file "~/.emacs.d/auto-generated-customized-settings.el")
 (load-file custom-file)
 
-;;; init.el ends here.
+(defun tareifz/load-only-theme ()
+  "Disable all themes and then load a single theme interactively."
+  (interactive)
+  (while custom-enabled-themes
+    (disable-theme (car custom-enabled-themes)))
+  (call-interactively 'load-theme))
+
+(defun tareifz/kill-line ()
+  "Killing current line with the new line character, and put the cursor at the beginning of the line."
+  (interactive)
+  (let (line-begin line-end)
+    (setq line-begin (line-beginning-position))
+    (setq line-end (if (= (point-max) (line-end-position))
+                       (progn
+                         (line-end-position))
+                     (progn
+                       (forward-line 1)
+                       (line-beginning-position))))
+    (kill-region line-begin line-end)
+    (beginning-of-line))
+  )
+
+(bind-key* "C-k" 'tareifz/kill-line)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;; (load-file "~/.emacs.d/themes/tareifz-basic-theme.el")
+;; (load-file "~/.emacs.d/themes/tareifz-shadows-theme.el")
+
+;; (use-package sourcerer-theme)
+;; (use-package dracula-theme)
+;; (use-package atom-one-dark-theme)
+(use-package hydandata-light-theme)
+
+(use-package org)
+
+(use-package try)
+
+(use-package smartparens
+  :config
+  (smartparens-global-mode t))
+
+(use-package rainbow-mode
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-mode))
+
+(use-package rainbow-delimiters
+  :requires rainbow-mode
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+(use-package aggressive-indent
+  :config
+  (global-aggressive-indent-mode t))
+
+(use-package beacon
+  :custom
+  (beacon-mode t "turn on beacon mode.")
+  (beacon-blink-when-focused t "let the cursor blink when focused."))
+
+(use-package smex
+  :bind (("M-x" . smex))
+  :config (smex-initialize))
+
+(use-package company
+  :config
+  (global-company-mode t))
+
+(use-package rust-mode
+  :custom
+  (company-tooltip-align-annotations t))
+
+;; rustup component add rust-src
+;; cargo install racer
+(use-package racer
+  :requires rust-mode
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode))
+
+(use-package multiple-cursors
+  :config
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
+
+(use-package switch-window
+  :config
+  (global-set-key (kbd "C-x o") 'switch-window))
+
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+
+(use-package js2-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+
+(use-package electric-operator
+  :config
+  (add-hook 'rust-mode-hook #'electric-operator-mode)
+  (add-hook 'javascript-mode-hook #'electric-operator-mode)
+  (electric-operator-add-rules-for-mode 'rust-mode
+                                        (cons "->" " -> ")))
+
+;; M-x httpd-start
+;; M-x impatient-mode
+;; http://localhost:8080/imp/
+(use-package impatient-mode)
+
+(use-package all-the-icons)
+;; M-x all-the-icons-install-fonts
+
+(use-package neotree
+  :bind(("<f5>" . neotree-toggle))
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq-default neo-show-hidden-files t))
+
+(use-package telephone-line
+  :config
+  (telephone-line-mode 1))
+
+(use-package which-key
+  :config
+  (which-key-mode)
+  (which-key-setup-side-window-right-bottom))
+
+(use-package expand-region
+  :bind ("C-=" . er/expand-region))
+
+(use-package flycheck
+  :init (global-flycheck-mode)
+  :config
+  (flycheck-add-mode 'javascript-eslint 'web-mode))
+
+;; annoying with long error messages (rustc errors)
+(use-package flycheck-inline
+  :requires (flycheck)
+  :config (flycheck-inline-mode 1))
+
+(use-package flycheck-rust
+  :requires (flycheck)
+  :hook (rust-mode . flycheck-rust-setup))
+
+;; C-c C-b === eval-buffer
+;; C-c C-c === eval-definition.
+(use-package geiser
+  :config
+  (setq geiser-active-implementations '(guile)))
+
+(use-package flymd
+  :config
+  (setq-default flymd-output-directory "~/.flymd/"))
+
+(use-package helm
+  :init
+  (setq helm-mode-fuzzy-match        t
+        helm-buffers-fuzzy-matching  t
+        helm-recentf-fuzzy-match     t
+        helm-M-x-fuzzy-match         t
+        helm-full-frame              nil
+        helm-ff-guess-ffap-urls      nil
+        helm-ff-guess-ffap-filenames nil
+        helm-highlight-matches-around-point-max-lines 0)
+  :bind (("M-x" . helm-M-x)
+         ("C-x b" . helm-buffers-list)
+         ("C-x C-f" . helm-find-files)
+         ("C-x C-d" . helm-browse-project)
+         ("C-c f" . helm-recentf)
+         ("M-y" . helm-show-kill-ring)
+         :map helm-map
+         ("<tab>" . helm-execute-persistent-action) ;; make tab complete action
+         ("C-i" . helm-execute-persistent-action)
+         ("C-z" . helm-select-action)
+         ))
+
+;;(global-set-key (kbd "<f5> s")                       'helm-find)
+
+(use-package helm-ls-git
+  :requires (helm))
+
+;; [C-j] to expand
+(use-package emmet-mode
+  :hook (web-mode . emmet-mode))
+
+(use-package diff-hl
+  :config
+  (global-diff-hl-mode)
+  (diff-hl-flydiff-mode))
