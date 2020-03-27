b79f355440b9dfd45525da828e17dfa314282666
tareifz <root@tareifz.me>
2020-03-27 03:59:28 +0300

    clean Emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,6 @@
 (require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("marmalade" . "https://marmalade-repo.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
@@ -18,62 +17,6 @@
 
 (setq user-full-name "Tareif Al-Zamil"
       user-mail-address "root@tareifz.me")
-
-(defun server-shutdown ()
-  "Save buffers, Quit, and Shutdown (kill) server"
-  (interactive)
-  (save-some-buffers)
-  (kill-emacs)
-  )
-
-;; save & shutdown when we get an "end of session" signal on dbus
-(require 'dbus)
-
-(defun my-register-signals (client-path)
-  "Register for the 'QueryEndSession' and 'EndSession' signals from
-Gnome SessionManager.
-
-When we receive 'QueryEndSession', we just respond with
-'EndSessionResponse(true, \"\")'.  When we receive 'EndSession', we
-append this EndSessionResponse to kill-emacs-hook, and then call
-kill-emacs.  This way, we can shut down the Emacs daemon cleanly
-before we send our 'ok' to the SessionManager."
-  (setq my-gnome-client-path client-path)
-  (let ( (end-session-response (lambda (&optional arg)
-                                 (dbus-call-method-asynchronously
-                                  :session "org.gnome.SessionManager" my-gnome-client-path
-                                  "org.gnome.SessionManager.ClientPrivate" "EndSessionResponse" nil
-                                  t "") ) ) )
-         (dbus-register-signal
-          :session "org.gnome.SessionManager" my-gnome-client-path
-          "org.gnome.SessionManager.ClientPrivate" "QueryEndSession"
-          end-session-response )
-         (dbus-register-signal
-          :session "org.gnome.SessionManager" my-gnome-client-path
-          "org.gnome.SessionManager.ClientPrivate" "EndSession"
-          `(lambda (arg)
-             (add-hook 'kill-emacs-hook ,end-session-response t)
-             (kill-emacs) ) ) ) )
-
-;; DESKTOP_AUTOSTART_ID is set by the Gnome desktop manager when emacs
-;; is autostarted.  We can use it to register as a client with gnome
-;; SessionManager.
-(dbus-call-method-asynchronously
- :session "org.gnome.SessionManager"
- "/org/gnome/SessionManager"
- "org.gnome.SessionManager" "RegisterClient" 'my-register-signals
- "Emacs server" (getenv "DESKTOP_AUTOSTART_ID"))
-
-(add-hook 'after-make-frame-functions
-          (lambda (frame)
-                  (with-selected-frame frame
-                                       (progn (menu-bar-mode -1)
-                                       (tool-bar-mode -1)
-                                       (toggle-scroll-bar -1)
-                                       (global-linum-mode t)
-                                       (setq-default linum-format " %d ")
-                                       (set-frame-font "Fira Code")
-                                       (set-face-attribute 'default nil :height 140)))))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
@@ -128,26 +71,6 @@ before we send our 'ok' to the SessionManager."
 
 (setq-default tab-always-indent nil)
 
-(ido-mode 1)
-(ido-everywhere 1)
-
-(use-package ido-completing-read+
-  :config
-  (ido-ubiquitous-mode 1))
-
-(use-package ido-vertical-mode
-  :config
-  (ido-vertical-mode 1)
-  :custom
-  (ido-vertical-define-keys 'C-n-and-C-p-only))
-
-(use-package flx-ido
-  :config
-  (flx-ido-mode 1)
-  :custom
-  (ido-enable-flex-matching t)
-  (ido-use-faces nil))
-
 (setq make-backup-files nil)
 (setq backup-inhibited t)
 (setq auto-save-default nil)
@@ -171,13 +94,7 @@ before we send our 'ok' to the SessionManager."
 (setq bookmark-save-flag 1)
 
 (set-frame-font "Fira Code")
-;; (set-frame-font "More Perfect DOS VGA")
-;; (set-frame-font "Unifont")
-;; (set-frame-font "Terminus:antialias=none")
 (set-face-attribute 'default nil :height 140)
-;; (set-frame-font "Monaco")
-;; (set-frame-font "Gohu GohuFont")
-;; (set-face-attribute 'default nil :height 120)
 
 (setq-default custom-file "~/.emacs.d/auto-generated-customized-settings.el")
 (load-file custom-file)
@@ -207,18 +124,7 @@ before we send our 'ok' to the SessionManager."
 (bind-key* "C-k" 'tareifz/kill-line)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;; (load-file "~/.emacs.d/themes/tareifz-basic-theme.el")
-;; (load-file "~/.emacs.d/themes/tareifz-shadows-theme.el")
-;; (load-file "~/.emacs.d/themes/tareifz-shadow-theme.el")
 
-;; (use-package sourcerer-theme)
-  ;; (use-package dracula-theme)
-  ;; (use-package atom-one-dark-theme)
-  ;; (use-package hydandata-light-theme)
-  ;; (use-package panda-theme)
-  ;; (use-package hemera-theme)
-  ;; (use-package monokai-pro-theme)
-  ;; (use-package cherry-blossom-theme)
 (use-package modus-operandi-theme)
 
 (defun insert-crystal-template()
@@ -227,9 +133,16 @@ before we send our 'ok' to the SessionManager."
 
 (add-hook 'crystal-mode-hook 'insert-crystal-template)
 
-(use-package org)
+(use-package org
+  :config
+  (setq org-confirm-babel-evaluate nil)
+  (setq org-src-fontify-natively t))
 
 (use-package try)
+
+(use-package rainbow-mode
+  :hook
+  (prog-mode . rainbow-mode))
 
 (use-package rainbow-delimiters
   :requires rainbow-mode
@@ -258,26 +171,6 @@ before we send our 'ok' to the SessionManager."
   :bind
   ("C-x o" . switch-window))
 
-(use-package web-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
-
-(use-package js2-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
-
-;; M-x httpd-start
-;; M-x impatient-mode
-;; http://localhost:8080/imp/
-(use-package impatient-mode)
-
 (use-package which-key
   :config
   (which-key-mode)
@@ -299,55 +192,14 @@ before we send our 'ok' to the SessionManager."
 (with-eval-after-load 'rust-mode
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
-(use-package flymd
-  :custom
-  (flymd-output-directory "~/.flymd/"))
-
-;; [C-j] to expand
-(use-package emmet-mode
-  :hook (web-mode . emmet-mode))
-
 (use-package diff-hl
   :config
   (global-diff-hl-mode)
   (diff-hl-flydiff-mode))
 
-(use-package prettier-js
-  :hook (js2-mode . prettier-js-mode)
-  :config
-  (setq prettier-js-args '(
-    "--trailing-comma" "es5")))
-
 (use-package crystal-mode)
 
-(use-package apib-mode
-  :config
-  (add-to-list 'auto-mode-alist '("\\.apib\\'" . apib-mode)))
-
-(use-package eyebrowse
-  :config
-  (eyebrowse-mode t))
-
-(use-package indent-guide
-  :config
-  (indent-guide-global-mode))
-
-(use-package all-the-icons)
-
-(use-package neotree
-  :bind
-  ("<f5>" . neotree-toggle)
-  :config
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
-
 (use-package magit)
-
-(use-package spaceline-config
-  :ensure spaceline
-  :config
-  (setq powerline-default-separator 'bar
-        spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
-(spaceline-spacemacs-theme))
 
 (use-package hl-todo
   :config
@@ -395,3 +247,14 @@ before we send our 'ok' to the SessionManager."
 (use-package restclient
   :config
     (add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode)))
+
+(use-package ivy
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  (ivy-mode 1))
+
+(use-package counsel
+  :config
+  (counsel-mode))
